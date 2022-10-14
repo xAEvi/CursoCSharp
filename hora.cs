eb8729892c78df35c94677b8ad5c0a04af0500b3
xAEvi <105865102+xAEvi@users.noreply.github.com>
2022-10-14 14:38:49 -0500

    Create hora.cs

---
+++ hora.cs
@@ -0,0 +1,6 @@
+Console.WriteLine("Escriba la hora:");
+string hora = Console.ReadLine();
+Console.WriteLine("Escribe los minutos:");
+string minutos = Console.ReadLine();
+
+Console.WriteLine("La hora es: " + hora + ":" + minutos);
